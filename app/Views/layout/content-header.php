444657d8e2baa488c22d7aaf60fc7e64a89dd843
Sajib Adhikary <tosajibadhi@gmail.com>
2023-12-07 14:43:00 +0600

    initial app layout added

---
+++ app/Views/layout/content-header.php
@@ -0,0 +1,8 @@
+<section class="content-header">
+    <h1>
+        <?= $title ?? null ?>
+        <small><?= $titleDescription ?? null ?></small>
+    </h1>
+
+<?php //echo $this->include('layout\content-header-breadcrumb') ?>
+</section>
---
+++ app/Views/layout/header-navbar.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-static-top" role="navigation">
+    <!-- Sidebar toggle button-->
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+    </a>
+    <!-- Navbar Right Menu -->
+</nav>
