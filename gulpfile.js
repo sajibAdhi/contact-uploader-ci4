8b8e10341022500dcc735abb0984b634263d4f7c
Sajib Adhikary <tosajibadhi@gmail.com>
2023-12-17 10:51:54 +0600

    FEAT: webpack test

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('copy-adminlte', function () {
-    return gulp.src(['node_modules/admin-lte/dist/**']) // source of the files
-        .pipe(gulp.dest('public/assets/admin-lte')); // destination of the files
-});
-
-gulp.task('default', gulp.series('copy-adminlte'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+};
