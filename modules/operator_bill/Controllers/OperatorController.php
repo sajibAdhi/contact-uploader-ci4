00432a9c781945d1268a44033ba56906045b1d4c
Sajib Adhikary <tosajibadhi@gmail.com>
2024-01-17 13:19:10 +0600

    operator edit & update
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- modules/operator_bill/Controllers/OperatorController.php
+++ modules/operator_bill/Controllers/OperatorController.php
@@ -27,7 +27,7 @@ class OperatorController extends BaseController
 
     public function create(): string
     {
-        return operator_bill_view('operator\create', [
+        return operator_bill_view('operator\form', [
             'title' => 'Add Operator',
         ]);
     }
@@ -62,13 +62,44 @@ class OperatorController extends BaseController
         return redirect()->route('operator_bill.operator.index')->with('success', 'Operator created successfully');
     }
 
-    public function edit(int $id)
+    public function edit(int $id): string
     {
         $operator = $this->operatorModel->find($id);
 
-        return operator_bill_view('operator\edit', [
+        return operator_bill_view('operator\form', [
             'title' => 'Edit Operator',
+            'action' => 'edit',
             'operator' => $operator,
         ]);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function update(int $id)
+    {
+        $rules = [
+            'operator_name' => 'required',
+            'operator_address' => 'required',
+            'operator_phone' => 'required',
+            'operator_email' => 'required|valid_email',
+        ];
+
+        if (!$this->validate($rules)) {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+
+        // Get the validated data
+        $data = [
+            'name' => $this->request->getPost('operator_name'),
+            'address' => $this->request->getPost('operator_address'),
+            'phone' => $this->request->getPost('operator_phone'),
+            'email' => $this->request->getPost('operator_email'),
+        ];
+
+        $this->operatorModel->update($id, $data);
+
+        // Redirect the user back to the form with a success message
+        return redirect()->route('operator_bill.operator.index')->with('success', 'Operator updated successfully');
     }
 }
--- modules/operator_bill/Views/operator/form.php
+++ modules/operator_bill/Views/operator/form.php
@@ -4,9 +4,15 @@
     <!-- Operator Create Section -->
     <div class="box box-primary">
         <div class="box-header with-border">
-            <h3 class="box-title"><?= 'Add Operator' ?></h3>
+            <h3 class="box-title"><?= (($action ?? null) == 'edit') ? 'Edit' : 'Add' ?> Operator</h3>
         </div>
         <?= form_open(current_url(), ['class' => 'form-horizontal']) ?>
+
+        <?php if (validation_errors()): ?>
+            <div class="alert alert-danger">
+                <?= validation_list_errors() ?>
+            </div>
+        <?php endif; ?>
 
         <div class="box-body">
 
@@ -14,7 +20,8 @@
             <div class="form-group">
                 <label for="operator_name" class="control-label col-sm-3">Operator Name:</label>
                 <div class="col-sm-9">
-                    <input type="text" class="form-control" name="operator_name" id="operator_name" required>
+                    <input type="text" class="form-control" name="operator_name" id="operator_name"
+                           value="<?= set_value('operator_name', $operator->name ?? null) ?>" required>
                 </div>
             </div>
 
@@ -22,7 +29,8 @@
             <div class="form-group">
                 <label for="operator_address" class="control-label col-sm-3">Operator Address:</label>
                 <div class="col-sm-9">
-                    <input type="text" class="form-control" name="operator_address" id="operator_address" required>
+                    <input type="text" class="form-control" name="operator_address" id="operator_address"
+                           value="<?= set_value('operator_address', $operator->address ?? null) ?>" required>
                 </div>
             </div>
 
@@ -30,7 +38,8 @@
             <div class="form-group">
                 <label for="operator_phone" class="control-label col-sm-3">Operator Phone:</label>
                 <div class="col-sm-9">
-                    <input type="text" class="form-control" name="operator_phone" id="operator_phone" required>
+                    <input type="text" class="form-control" name="operator_phone" id="operator_phone"
+                           value="<?= set_value('operator_phone', $operator->phone ?? null) ?>" required>
                 </div>
             </div>
 
@@ -38,9 +47,11 @@
             <div class="form-group">
                 <label for="operator_email" class="control-label col-sm-3">Operator Email:</label>
                 <div class="col-sm-9">
-                    <input type="email" class="form-control" name="operator_email" id="operator_email" required>
+                    <input type="email" class="form-control" name="operator_email" id="operator_email"
+                           value="<?= set_value('operator_email', $operator->email ?? null) ?>" required>
                 </div>
             </div>
+
         </div>
 
         <?= view_cell(\App\Cells\FormSubmitCell::class, [
