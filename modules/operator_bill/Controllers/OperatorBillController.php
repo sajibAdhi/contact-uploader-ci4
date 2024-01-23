15374c9d7b3643e748acf1bc9cae27110e8c0b1a
Sajib Adhikary <tosajibadhi@gmail.com>
2024-01-23 12:33:54 +0600

    ref: Operator
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- modules/operator_bill/Controllers/OperatorBillController.php
+++ modules/operator_bill/Controllers/OperatorBillController.php
@@ -4,6 +4,8 @@ namespace OperatorBill\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\RedirectResponse;
+use OperatorBill\Constants\OperatorTypeConstant;
+use OperatorBill\Constants\SBNConstant;
 use OperatorBill\Services\OperatorBillService;
 use ReflectionException;
 
@@ -28,6 +30,8 @@ class OperatorBillController extends BaseController
     {
         return operator_bill_view('create', [
             'title' => 'Add Operator Bill',
+            'sbnList' => SBNConstant::all(),
+            'operatorTypes' => OperatorTypeConstant::all(),
             'operators' => $this->operatorBillService->operatorModel->findAll(),
         ]);
     }
@@ -37,18 +41,22 @@ class OperatorBillController extends BaseController
      */
     public function store(): RedirectResponse
     {
-        /** Validate The Data */
-        $this->storeValidation();
+        try {
+            /** Validate The Data */
+            $this->storeValidation();
 
-        // If the validation passes, then get the posted data
-        $postData = $this->request->getPost();
-        $files = $this->request->getFiles();
-        // Insert the posted data into the database
-        if ($this->operatorBillService->store($postData, $files['file_upload'])) {
-            return redirect()->route('operator_bill.index')->with('success', 'Operator Bill Created Successfully');
+            // If the validation passes, then get the posted data
+            $postData = $this->request->getPost();
+            $files = $this->request->getFiles();
+            // Insert the posted data into the database
+            if ($this->operatorBillService->store($postData, $files['file_upload'])) {
+                return redirect()->route('operator_bill.index')->with('success', 'Operator Bill Created Successfully');
+            }
+
+            return redirect()->back()->withInput()->with('_ci_validation_errors', $this->operatorBillService->errors());
+        } catch (\Exception $e) {
+            return redirect()->back()->withInput()->with('error', $e->getMessage());
         }
-
-        return redirect()->back()->withInput()->with('error', 'Something went wrong');
     }
 
     public function edit(int $id): string
