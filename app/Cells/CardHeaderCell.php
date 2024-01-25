a6927d1d5c7afc845e2cdbf059811c2b769c19e9
Sajib Adhikary <tosajibadhi@gmail.com>
2024-01-25 18:33:57 +0600

    feat: card-header
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

---
+++ app/Cells/CardHeaderCell.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Cells;
+
+use CodeIgniter\View\Cells\Cell;
+
+class CardHeaderCell extends Cell
+{
+    public string $title;
+}
---
+++ app/Cells/card_header.php
@@ -0,0 +1,3 @@
+<div class="card-header">
+    <h3 class="card-title"><?= $title ?? null ?></h3>
+</div>
