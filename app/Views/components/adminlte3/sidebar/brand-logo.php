4f82c91715ff126c72c438e984ae2259841cdb9e
Sajib Adhikary <tosajibadhi@gmail.com>
2024-01-29 13:54:37 +0600

    fix: brand-logo.php
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- app/Views/components/adminlte3/sidebar/brand-logo.php
+++ app/Views/components/adminlte3/sidebar/brand-logo.php
@@ -1,4 +1,4 @@
-<a href="index3.html" class="brand-link">
+<a href="<?= base_url() ?>" class="brand-link">
     <img src="<?= base_url(\App\Constants\ApplicationConstant::ICON) ?>"
          alt="<?= \App\Constants\ApplicationConstant::NAME ?>"
          class="brand-image img-responsive elevation-0"
