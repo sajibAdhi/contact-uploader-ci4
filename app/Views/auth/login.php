eb8d40ee1402ec6c8648531d26d7a675730ab1bf
Sajib Adhikary <tosajibadhi@gmail.com>
2024-02-05 11:42:06 +0600

    wip: login ui
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,73 +1,107 @@
-<?= $this->extend(config('Auth')->views['layout']) ?>
+<?= $this->extend('layout/auth') ?>
 
 <?= $this->section('title') ?><?= lang('Auth.login') ?> <?= $this->endSection() ?>
 
 <?= $this->section('main') ?>
 
-    <div class="container d-flex justify-content-center p-5">
-        <div class="card col-12 col-md-5 shadow-sm">
-            <div class="card-body">
-                <h5 class="card-title mb-5"><?= lang('Auth.login') ?></h5>
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a href="<?= base_url() ?>" class="h1">
+                <b>
+                    <img src="<?= base_url(\App\Constants\ApplicationConstant::ICON) ?>"
+                         alt="<?= \App\Constants\ApplicationConstant::NAME ?>">
+                </b>
+                <?= \App\Constants\ApplicationConstant::SHORT_NAME ?>
+            </a>
+        </div>
+        <div class="card-body">
+            <p class="login-box-msg">Login to start your session</p>
+            <?php if (session('error') !== null) : ?>
+                <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+            <?php elseif (session('errors') !== null) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php if (is_array(session('errors'))) : ?>
+                        <?php foreach (session('errors') as $error) : ?>
+                            <?= $error ?>
+                            <br>
+                        <?php endforeach ?>
+                    <?php else : ?>
+                        <?= session('errors') ?>
+                    <?php endif ?>
+                </div>
+            <?php endif ?>
 
-                <?php if (session('error') !== null) : ?>
-                    <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-                <?php elseif (session('errors') !== null) : ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php if (is_array(session('errors'))) : ?>
-                            <?php foreach (session('errors') as $error) : ?>
-                                <?= $error ?>
-                                <br>
-                            <?php endforeach ?>
-                        <?php else : ?>
-                            <?= session('errors') ?>
-                        <?php endif ?>
-                    </div>
-                <?php endif ?>
-
-                <?php if (session('message') !== null) : ?>
+            <?php if (session('message') !== null) : ?>
                 <div class="alert alert-success" role="alert"><?= session('message') ?></div>
-                <?php endif ?>
+            <?php endif ?>
 
-                <form action="<?= url_to('login') ?>" method="post">
-                    <?= csrf_field() ?>
+            <?= form_open(url_to('login')) ?>
+            <!-- Email -->
+            <div class="input-group mb-3">
+                <input type="email" class="form-control" id="email" name="email"
+                       value="<?= old('email') ?>" inputmode="email"
+                       autocomplete="email" placeholder="<?= lang('Auth.email') ?>" required>
+                <div class="input-group-append">
+                    <label for="email" class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                    </label>
+                </div>
+            </div>
 
-                    <!-- Email -->
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingEmailInput" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required>
-                        <label for="floatingEmailInput"><?= lang('Auth.email') ?></label>
-                    </div>
 
-                    <!-- Password -->
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="floatingPasswordInput" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required>
-                        <label for="floatingPasswordInput"><?= lang('Auth.password') ?></label>
-                    </div>
+            <!-- Password -->
+            <div class="input-group mb-3">
+                <input type="password" class="form-control" id="password" name="password"
+                       inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>"
+                       required>
+                <div class="input-group-append">
+                    <label for="password" class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </label>
+                </div>
+            </div>
 
-                    <!-- Remember me -->
+            <div class="row">
+
+                <!-- Remember me -->
+                <div class="col-8">
                     <?php if (setting('Auth.sessionConfig')['allowRemembering']): ?>
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')): ?> checked<?php endif ?>>
+                        <div class="icheck-primary">
+                            <input type="checkbox" name="remember"
+                                   id="remember" <?= old('remember') ? 'checked' : null ?> >
+                            <label for="remember">
                                 <?= lang('Auth.rememberMe') ?>
                             </label>
                         </div>
                     <?php endif; ?>
+                </div>
+                <!-- /.col -->
 
-                    <div class="d-grid col-12 col-md-8 mx-auto m-3">
-                        <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.login') ?></button>
-                    </div>
-
-                    <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
-                        <p class="text-center"><?= lang('Auth.forgotPassword') ?> <a href="<?= url_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
-                    <?php endif ?>
-
-                    <?php if (setting('Auth.allowRegistration')) : ?>
-                        <p class="text-center"><?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>"><?= lang('Auth.register') ?></a></p>
-                    <?php endif ?>
-
-                </form>
+                <div class="col-4">
+                    <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.login') ?></button>
+                </div>
+                <!-- /.col -->
             </div>
+            <?= form_close() ?>
+
+            <!--            --><?php //= $this->include('App\Modules\Shield\Views\components\social-auth-links') ?>
+
+            <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
+                <p class="text-center"><?= lang('Auth.forgotPassword') ?> <a
+                            href="<?= url_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
+            <?php endif ?>
+
+            <?php if (setting('Auth.allowRegistration')) : ?>
+                <p class="text-center"><?= lang('Auth.needAccount') ?> <a
+                            href="<?= url_to('register') ?>"><?= lang('Auth.register') ?></a></p>
+            <?php endif ?>
         </div>
+        <!-- /.card-body -->
     </div>
+    <!-- /.card -->
+</div>
+<!-- /.login-box -->
 
 <?= $this->endSection() ?>
