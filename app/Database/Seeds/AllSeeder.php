5640eb7f01244be27e3e5ac7d88594d1339892e8
Sajib Adhikary <tosajibadhi@gmail.com>
2024-02-29 12:04:23 +0600

    :bug: AllSeeder.php
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- app/Database/Seeds/AllSeeder.php
+++ app/Database/Seeds/AllSeeder.php
@@ -2,15 +2,13 @@
 
 namespace App\Database\Seeds;
 
-use App\Modules\OperatorBill\Database\Seeds\OperatorSeeder;
 use CodeIgniter\Database\Seeder;
 
 class AllSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(UserSeeder::class);
+//        $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
-        $this->call(OperatorSeeder::class);
     }
 }
